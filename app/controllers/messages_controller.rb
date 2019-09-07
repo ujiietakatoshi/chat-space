a2009531b793270d7020b1d60e7de284c903569f
ujiietakatoshi <fadeoutinu@gmail.com>
2019-09-07 20:54:17 +0900

    revise7

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,9 +9,8 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
       respond_to do |format|
-        format.html { redirect_to group_messages_path(params[:tweet_id])  }
+        format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
         format.json
     end
     else
