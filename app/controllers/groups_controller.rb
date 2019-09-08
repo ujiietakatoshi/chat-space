a1baf89c0e20e662718648538762343d80d67603
ujiietakatoshi <fadeoutinu@gmail.com>
2019-09-08 17:33:54 +0900

    revise 2

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,19 +29,7 @@ class GroupsController < ApplicationController
     else
       render :new
     end
-
   end
-
-    def edit
-    end
-    
-    def update
-      if @group.update(group_params)
-        redirect_to group_messages_path(@group), notice: 'グループを編集しました'
-      else
-        render :edit
-      end
-    end
 
   private
 
