7b098e0a100984d810cba64c15d94445ec4a8b4d
ujiietakatoshi <fadeoutinu@gmail.com>
2019-09-11 00:00:18 +0900

    rerevise2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?) and id != ?',"#{params[:name]}%", current_user)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
     respond_to do |format|
-      format.html
       format.json
    end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
-  resources :users, only: [:index]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
