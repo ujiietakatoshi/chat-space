c43380614b5a8ba7305f3c37d68a557b7435756a
ujiietakatoshi <fadeoutinu@gmail.com>
2019-09-12 23:24:08 +0900

    RV0

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -15,7 +15,7 @@ $(function() {
 
   function appendUserToMemberList(name, user_id) {
     var html = 
-      `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+      `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-1'>
         <input name='group[user_ids][]' type='hidden' value=${ user_id }>
         <p class='chat-group-user__name'>${ name }</p>
         <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
