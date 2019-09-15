d5a9643280d997af51bae90424d6df0bd3939458
ujiietakatoshi <fadeoutinu@gmail.com>
2019-09-15 17:09:51 +0900

    rere2

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -7,7 +7,7 @@ $(function() {
     var html = 
       `<div class="chat-group-user clearfix">
           <p class="chat-group-user__name">${ user.name }</p>
-          <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name=${ user.name }>追加</a>
+          <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name=${ user.name }>追加</div>
       </div>`
 　　 search_list.append(html);
 　　 return html;
@@ -15,10 +15,10 @@ $(function() {
 
   function appendUserToMemberList(name, user_id) {
     var html = 
-      `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-1'>
-        <input name='group[user_ids][]' type='hidden' value=${ user_id }>
-        <p class='chat-group-user__name'>${ name }</p>
-        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+      `<div class="chat-group-user">
+        <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+        <p class="chat-group-user__name">${ name }</p>
+        <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
       </div>`
 　　 member_list.append(html);
   }
